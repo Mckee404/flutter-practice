11bea96d44cec2038fcd94cb0f1ff4f97ccef044
Mckee404 <souta.hss.2004@gmail.com>
2024-12-23 22:06:43 +0900

    編集機能が正しくキャンセルされるよう修正

--- lib/class_todo.dart
+++ lib/class_todo.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Todo{
-  String title="";
-  String description="";
-  Color color = Colors.amber;
+class Todo {
+  final String title;
+  final String description;
+  final Color color;
 
-
-  Todo();
+  Todo({
+    required this.title,
+    required this.description,
+    required this.color,
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,3 @@
-//最後の引数までカンマを入れて自動整形された形で整えておく
-//setStateしなくてもいいところはsetStateしないほうがパフォーマンスに優れる
-//クラスにはkeyを設定しておいたほうがよいこともある
-//Widget型の変数をif文で制御すると省略できる部分がある
-//constをつけたほうがパフォーマンスに優れる
-//テキストボックスの中身を逐一変数に保存するのはあまり良くない
-
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/class_todo.dart';
 import 'todo_add_page.dart';
@@ -24,28 +17,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: ToDoListPage(),
+      home: const ToDoListPage(),
     );
   }
 }
 
 class ToDoListPage extends StatefulWidget {
+  const ToDoListPage({super.key});
   @override
-  _ToDoListPageState createState() => _ToDoListPageState();
+  State createState() => _ToDoListPageState();
 }
 
 class _ToDoListPageState extends State<ToDoListPage> {
   List<Todo> toDoList = [];
   void goToTodoAddPage() async {
     final newTodo = await Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) {
-        return ToDoAddPage();
-      }),
+      MaterialPageRoute(
+        builder: (context) {
+          return const ToDoAddPage();
+        },
+      ),
     );
     if (newTodo != null) {
-      setState(() {
-        toDoList.add(newTodo);
-      });
+      setState(
+        () {
+          toDoList.add(newTodo);
+        },
+      );
     }
   }
 
@@ -59,25 +57,32 @@ class _ToDoListPageState extends State<ToDoListPage> {
         itemCount: toDoList.length,
         itemBuilder: (context, index) {
           return TodoItem(
-              item: toDoList[index],
-              onDelete: () {
-                setState(() {
+            item: toDoList[index],
+            onDelete: () {
+              setState(
+                () {
                   toDoList.removeAt(index);
-                });
-              });
+                },
+              );
+            },
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final newTodo = await Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              return ToDoAddPage();
-            }),
+            MaterialPageRoute(
+              builder: (context) {
+                return const ToDoAddPage();
+              },
+            ),
           );
           if (newTodo != null) {
-            setState(() {
-              toDoList.add(newTodo);
-            });
+            setState(
+              () {
+                toDoList.add(newTodo);
+              },
+            );
           }
         },
         child: const Icon(Icons.add),
