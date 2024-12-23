741a506e89af4c7f9e581d9379de024e0e61c2ff
Mckee404 <souta.hss.2004@gmail.com>
2024-12-23 16:46:37 +0900

    編集機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-//タイトル、説明、タスクの色を選択（カラーピッカー、外部のパッケージ）
-//辞書型,クラス
-
+//最後の引数までカンマを入れて自動整形された形で整えておく
+//setStateしなくてもいいところはsetStateしないほうがパフォーマンスに優れる
+//クラスにはkeyを設定しておいたほうがよいこともある
+//Widget型の変数をif文で制御すると省略できる部分がある
+//constをつけたほうがパフォーマンスに優れる
+//テキストボックスの中身を逐一変数に保存するのはあまり良くない
 
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/class_todo.dart';
@@ -33,17 +36,29 @@ class ToDoListPage extends StatefulWidget {
 
 class _ToDoListPageState extends State<ToDoListPage> {
   List<Todo> toDoList = [];
+  void goToTodoAddPage() async {
+    final newTodo = await Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) {
+        return ToDoAddPage();
+      }),
+    );
+    if (newTodo != null) {
+      setState(() {
+        toDoList.add(newTodo);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('リスト一覧'),
+        title: const Text('リスト一覧'),
       ),
       body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (context, index) {
-          return ToDoItem(
+          return TodoItem(
               item: toDoList[index],
               onDelete: () {
                 setState(() {
@@ -65,7 +80,7 @@ class _ToDoListPageState extends State<ToDoListPage> {
             });
           }
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
