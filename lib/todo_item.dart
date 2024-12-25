a24fdc8b2165ff959000196ded461c212fecb6ec
Mckee404 <souta.hss.2004@gmail.com>
2024-12-25 15:43:35 +0900

    カードにドラッグUIを追加

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -18,22 +18,19 @@ class TodoItem extends StatefulWidget {
 class _TodoItemState extends State<TodoItem> {
   late Todo currentState;
   late VoidCallback onDelete;
+  late final listTileSubTitle;
   @override
   void initState() {
     super.initState();
-
     currentState = widget.item;
     onDelete = widget.onDelete;
+    listTileSubTitle = currentState.description.isEmpty
+        ? null
+        : Text(currentState.description);
   }
 
   @override
   Widget build(BuildContext context) {
-    final Text? listTileSubTitle;
-    if (currentState.description.isEmpty) {
-      listTileSubTitle = null;
-    } else {
-      listTileSubTitle = Text(currentState.description);
-    }
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
@@ -55,39 +52,52 @@ class _TodoItemState extends State<TodoItem> {
           ),
           padding: const EdgeInsets.all(8.0),
           alignment: Alignment.centerLeft,
-          child: ListTile(
-            title: Text(currentState.title),
-            subtitle: listTileSubTitle,
-            trailing: Wrap(
-              spacing: 8,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () async {
-                    final returnValue = await Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return ToDoAddPage(
-                            modifiedItem: currentState,
+          child: Stack(
+            alignment: AlignmentDirectional.topCenter,
+            children: [
+              ListTile(
+                title: Text(currentState.title),
+                subtitle: listTileSubTitle,
+                trailing: Wrap(
+                  spacing: 8,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () async {
+                        final returnValue = await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return ToDoAddPage(
+                                modifiedItem: currentState,
+                              );
+                            },
+                          ),
+                        );
+                        if (returnValue is Todo) {
+                          setState(
+                            () {
+                              currentState = returnValue;
+                            },
                           );
-                        },
-                      ),
-                    );
-                    if (returnValue is Todo) {
-                      setState(
-                        () {
-                          currentState = returnValue;
-                        },
-                      );
-                    }
-                  },
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: onDelete,
+                    ),
+                  ],
                 ),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: onDelete,
+              ),
+              Container(
+                width: 80,
+                height: 4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
